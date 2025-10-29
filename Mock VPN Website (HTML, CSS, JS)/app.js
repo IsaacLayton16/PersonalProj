db4ae18e6290bbacdcde5930c707b7b3c29773f9
IsaacLayton16 <144192616+IsaacLayton16@users.noreply.github.com>
2025-10-28 18:02:14 -0700

    Update app.js

--- Mock VPN Website (HTML, CSS, JS)/app.js
+++ Mock VPN Website (HTML, CSS, JS)/app.js
@@ -1,1 +1,18 @@
+const menu = document.querySelector("#mobile-menu") 
+const menuLinks = document.querySelector('.navbar__menu')
+
+menu.addEventListener("click", function () {
+
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle("active");
+
+    console.log("It worked!");
+})
+
+// Function to make logos move
+
+const logos = document.querySelector(".rotating-logos__track").cloneNode(true)
+document.querySelector(".rotating-logos").appendChild(logos)
+
+
 
